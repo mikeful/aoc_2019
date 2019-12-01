62a018e90e23e64bbfafabce80e52c58947fd155
Mikko Niemikorpi <mikefulli@gmail.com>
2019-12-01 20:58:22 +0200

    Added day 1

---
+++ 01/part1.jl
@@ -0,0 +1,30 @@
+function calculate_fuel(mass)
+	Int(floor(mass / 3) - 2)
+end
+
+testCases = [
+	(12, 2)
+	(14, 2)
+	(1969, 654)
+	(100756, 33583)
+]
+for pair in testCases
+	mass, expected_fuelcost = pair
+	println(string(calculate_fuel(mass), " -> ", expected_fuelcost))
+end
+
+lines = open("part1.txt") do file
+    readlines(file)
+end
+println(lines)
+
+# https://docs.julialang.org/en/v1/base/arrays/index.html#Base.Broadcast.broadcast
+println(
+	sum(
+		calculate_fuel.(
+			parse.(
+				Int, lines
+			)
+		)
+	)
+)
---
+++ 01/part2.jl
@@ -0,0 +1,34 @@
+function calculate_fuel(mass)
+	fuel_cost = Int(floor(mass / 3) - 2)
+	if fuel_cost > 0
+	    return fuel_cost + calculate_fuel(fuel_cost)
+    else
+    	return 0
+	end
+end
+
+testCases = [
+	(14, 2)
+	(1969, 966)
+	(100756, 50346)
+]
+for pair in testCases
+	mass, expected_fuelcost = pair
+	println(string(calculate_fuel(mass), " -> ", expected_fuelcost))
+end
+
+lines = open("part1.txt") do file
+    readlines(file)
+end
+println(lines)
+
+# https://docs.julialang.org/en/v1/base/arrays/index.html#Base.Broadcast.broadcast
+println(
+	sum(
+		calculate_fuel.(
+			parse.(
+				Int, lines
+			)
+		)
+	)
+)
