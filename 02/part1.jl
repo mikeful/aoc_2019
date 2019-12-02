e707f48173ac87f2325b07088e4f128041ca7ceb
Mikko Niemikorpi <mikefulli@gmail.com>
2019-12-02 21:32:39 +0200

    Added solutions for day 2

---
+++ 02/part1.jl
@@ -0,0 +1,58 @@
+function compute(memory)
+	counter = 0
+	while true
+		# Julia Arrays start at index 1 instead of 0
+		memory_index = counter + 1
+
+		opcode = memory[memory_index]
+		if opcode == 1
+			number1_index = memory[memory_index + 1] + 1
+			number2_index = memory[memory_index + 2] + 1
+			destination_index = memory[memory_index + 3] + 1
+			number1 = memory[number1_index]
+			number2 = memory[number2_index]
+
+			memory[destination_index] = number1 + number2
+		elseif opcode == 2
+			number1_index = memory[memory_index + 1] + 1
+			number2_index = memory[memory_index + 2] + 1
+			destination_index = memory[memory_index + 3] + 1
+			number1 = memory[number1_index]
+			number2 = memory[number2_index]
+
+			memory[destination_index] = number1 * number2
+		elseif opcode == 99
+			return memory[1]
+		else
+			# Unkown opcode
+			return nothing
+		end
+
+		counter += 4
+	end
+end
+
+testCases = [
+	([1 9 10 3 2 3 11 0 99 30 40 50], 3500)
+	([1 0 0 0 99], 2)
+	([2 3 0 3 99], 2)
+	([2 4 4 5 99 0], 2)
+	([1 1 1 4 99 5 6 0 99], 30)
+]
+for pair in testCases
+	program, expected_first = pair
+	println(string(compute(program), " -> ", expected_first))
+end
+
+line = open("input.txt") do file
+	read(file, String)
+end
+println(line)
+
+program = parse.(Int, split(line, ","))
+program[2] = 12
+program[3] = 2
+
+@time result = compute(program)
+
+println(result)
---
+++ 02/part2.jl
@@ -0,0 +1,58 @@
+function compute(memory)
+	counter = 0
+	while true
+		# Julia Arrays start at index 1 instead of 0
+		memory_index = counter + 1
+
+		opcode = memory[memory_index]
+		if opcode == 1
+			number1_index = memory[memory_index + 1] + 1
+			number2_index = memory[memory_index + 2] + 1
+			destination_index = memory[memory_index + 3] + 1
+			number1 = memory[number1_index]
+			number2 = memory[number2_index]
+
+			memory[destination_index] = number1 + number2
+		elseif opcode == 2
+			number1_index = memory[memory_index + 1] + 1
+			number2_index = memory[memory_index + 2] + 1
+			destination_index = memory[memory_index + 3] + 1
+			number1 = memory[number1_index]
+			number2 = memory[number2_index]
+
+			memory[destination_index] = number1 * number2
+		elseif opcode == 99
+			return memory[1]
+		else
+			# Unkown opcode
+			return nothing
+		end
+
+		counter += 4
+	end
+end
+
+function search(original_program)
+	for noun in 0:99
+		for verb in 0:99
+			program = copy(original_program)
+			program[2] = noun
+			program[3] = verb
+
+			result = compute(program)
+			if result == 19690720
+			    return noun, verb
+			end
+		end
+	end
+end
+
+line = open("input.txt") do file
+	read(file, String)
+end
+println(line)
+
+original_program = parse.(Int, split(line, ","))
+@time noun, verb = search(original_program)
+
+println(100 * noun + verb)
